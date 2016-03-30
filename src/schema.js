3f575ccfddbf7126c3a64edca87bcf4d2b67ea8d
Fred Wang <namelos@outlook.com>
2016-03-30 18:34:31 +0800

    init

--- src/schema.js
+++ src/schema.js
@@ -1,51 +1,59 @@
 // import * as _ from 'underscore';
 
 // This is the Dataset in our blog
-// import PostsList from './data/posts';
+import PostsList from './data/posts';
 // import AuthorsList from './data/authors';
 // import {CommentList, ReplyList} from './data/comments';
 
 import {
-  // These are the basic GraphQL types
-  // GraphQLInt,
-  // GraphQLFloat,
-  GraphQLString,
-  // GraphQLList,
-  GraphQLObjectType,
-  // GraphQLEnumType,
+  GraphQLInt as Int,
+  GraphQLFloat as Float,
+  GraphQLString as String,
+  GraphQLList as List,
+  GraphQLObjectType as ObjectType,
+  GraphQLEnumType as EnumType,
+  GraphQLNonNull as NonNull,
+  GraphQLSchema as Schema
+} from 'graphql'
 
-  // This is used to create required fields and arguments
-  // GraphQLNonNull,
-
-  // This is the class we need to create the schema
-  GraphQLSchema
-} from 'graphql';
-
-/**
-  DEFINE YOUR TYPES BELOW
-**/
-
-// This is the Root Query
-const Query = new GraphQLObjectType({
-  name: 'BlogSchema',
-  description: 'Root of the Blog Schema',
+const Post = new ObjectType({
+  name: 'Post',
+  description: 'This represent a Post',
   fields: () => ({
-    echo: {
-      type: GraphQLString,
-      description: 'Echo what you enter',
-      args: {
-        message: {type: GraphQLString}
-      },
-      resolve: function(source, {message}) {
-        return `received ${message}`;
-      }
+    _id: {
+      type: new NonNull(String)
+    },
+    title: {
+      type: new NonNull(String),
+      resolve: ({ title }) => title || 'Does not exist'
+    },
+    content: {
+      type: String
     }
   })
-});
+})
 
-// The Schema
-const Schema = new GraphQLSchema({
-  query: Query
-});
+export default new Schema({
+  query: new ObjectType({
+    name: 'BlogSchema',
+    description: 'Root of the Blog Schema',
+    fields: () => ({
+      echo: {
+        type: String,
+        description: 'Echo what you enter',
+        args: {
+          message: {
+            type: String,
+            description: 'Give me a message'
+          }
+        },
+        resolve: (source, { message }) => `received ${ message }`
+      },
+      posts: {
+        type: new List(Post),
+        resolve: () => PostsList
+      }
+    })
+  })
+})
 
-export default Schema;
